451850c4eb4e8423352d38a75c7e23e02bb3ad51
Jay Hayes <ur@iamvery.com>
2016-07-15 15:24:39 -0500

    Move object initialization to beforeEach callback

--- test/fake_phoenix_channel_test.js
+++ test/fake_phoenix_channel_test.js
@@ -2,11 +2,15 @@ var {expect} = require('./helper');
 
 describe('FakePhoenixChannel', () => {
   var FakePhoenixChannel = require('./support/fake_phoenix_channel');
+  var channel;
+
+  beforeEach(() => {
+    channel = new FakePhoenixChannel();
+  });
 
   describe('#on', () => {
     it('is invoked when data arrives', (done) => {
       var data = {};
-      var channel = new FakePhoenixChannel();
 
       channel.on('does not matter', (payload) => {
         expect(payload).to.equal(data);
