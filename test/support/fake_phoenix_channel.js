f75658a0518c79219914c33dedd15a1f31514e5d
Jay Hayes <ur@iamvery.com>
2016-07-15 15:25:54 -0500

    Convert single line to multi-line

--- test/support/fake_phoenix_channel.js
+++ test/support/fake_phoenix_channel.js
@@ -4,6 +4,8 @@ FakePhoenixChannel.prototype.on = function(_event, callback) {
   this.callback = callback;
 };
 
-FakePhoenixChannel.prototype.synthesize = function(data) { this.callback(data); }
+FakePhoenixChannel.prototype.synthesize = function(data) {
+  this.callback(data);
+}
 
 module.exports = FakePhoenixChannel;
