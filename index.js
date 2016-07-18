dfa6439382083f762fe85255c0b3de1611c8b8fa
Jay Hayes <ur@iamvery.com>
2016-07-18 16:34:00 -0500

    Accidentally I es6
    Supposedly I could setup my environment to build es6, but i don't wanna
    shave the yak right now. So es5 it is for me.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 var Ratchet = require('./lib/ratchet');
 var PhoenixAdapter = require('./lib/ratchet/adapters/phoenix');
 
-module.exports = {Ratchet, PhoenixAdapter};
+module.exports = {Ratchet: Ratchet, PhoenixAdapter: PhoenixAdapter};
