9492607a066115591ea7979123aef4c8ef368527
Jay Hayes <ur@iamvery.com>
2016-09-12 09:58:24 -0500

    Update example with combined data

--- examples/index.js
+++ examples/index.js
@@ -2,7 +2,8 @@ var {Jank} = require('../index.js');
 
 var data = {
   counter: {count: "123"},
-  count: "456",
+  count: {content: "456", attrs: {_attrs_: true, className: "wat"}},
+  // TODO handle class -> className etc. conversion
   post: [
     {id: 1, title: "foo", body: "bar"},
     {id: 2, title: "stuff", body: "things"},
